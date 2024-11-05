853202749b232e2b0bad9955a77f5c9912c6e5c9
Sneha Rathoe <rathoresneha22@gmail.com>
2024-11-05 18:24:27 +0530

    Selenium

---
+++ README.md
@@ -0,0 +1,11 @@
+# Learning Selenium ATB7x
+
+Programs Containing Selenium Learning Resource.
+
+
+### Topics 
+1. What is Selenium?
+2. Features of Selenium?
+3. Why Selenium, Advantage, Disadvantage
+4. How to install Selenium, use it 
+5. Selenium Architecture (W3C, Json Wire protocol)
--- src/test/java/com/thetestingacademy/ex_selenium/Selenium001.java
+++ src/test/java/com/thetestingacademy/ex_selenium/Selenium001.java
@@ -1,2 +1,13 @@
-package com.thetestingacademy.ex_selenium;public class Selenium001 {
+package com.thetestingacademy.ex_selenium;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Selenium001 {
+    public static void main(String[] args) {
+
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://app.vwo.com");
+
+
+    }
 }
