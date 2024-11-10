d819c6551b526f3f9186a631dcaf00b9d49b579c
Sneha Rathoe <rathoresneha22@gmail.com>
2024-11-10 13:51:00 +0530

    Selenium

---
+++ src/test/java/com/thetestingacademy/ex_selenium/Selenium020.java
@@ -0,0 +1,26 @@
+package com.thetestingacademy.ex_selenium;
+
+import io.qameta.allure.Description;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class Selenium020 {
+
+
+    @Description("Verify <add details>")
+    @Test
+    public void test_sample() {
+
+
+
+
+
+
+    }
+}
